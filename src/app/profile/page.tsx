6e9d99fa7b448082ac049d29a36c1335d65894a9
Ritik <160255700+gritik418@users.noreply.github.com>
2025-02-18 23:19:07 +0530

    Profile page about section completed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,22 +1,87 @@
 "use client";
+import EditUserInfo from "@/components/EditUserInfo/EditUserInfo";
 import { selectUser } from "@/features/user/userSlice";
-import { JSX } from "react";
+import { Input, Textarea } from "@chakra-ui/react";
+import { JSX, useState } from "react";
 import { useSelector } from "react-redux";
+import { CiEdit } from "react-icons/ci";
+import { GrEdit } from "react-icons/gr";
 
 const Profile = (): JSX.Element => {
   const user: User | null = useSelector(selectUser);
+  const [allowEdit, setAllowEdit] = useState<boolean>(false);
 
   return (
     <div className="bg-white rounded-lg p-3 mt-4 mb-6">
-      <div className="mt-4">
-        <p className="text-lg text-gray-600 mt-2">{user?.bio}</p>
+      <div className="flex p-3 justify-end">
+        <div
+          onClick={() => setAllowEdit(!allowEdit)}
+          className="flex bg-gray-200 font-semibold cursor-pointer p-2 rounded-lg"
+        >
+          <CiEdit className="text-xl" />
+        </div>
       </div>
 
-      <div className="mt-4 flex justify-center">
-        <button className="p-2 px-6 bg-[var(--secondary)] font-medium text-white rounded-full">
-          Edit Profile
-        </button>
-      </div>
+      {allowEdit ? (
+        <EditUserInfo />
+      ) : (
+        <div className="my-4 grid grid-cols-1 md:grid-cols-2 justify-center flex-col gap-4 items-center">
+          <div className="flex rounded-lg flex-col p-2 gap-1">
+            <label htmlFor="" className="ml-2">
+              First Name
+            </label>
+            <Input
+              className="bg-gray-100 p-2 rounded-lg outline-none"
+              readOnly
+              value={user?.firstName}
+            />
+          </div>
+
+          <div className="flex rounded-lg flex-col p-2 gap-1">
+            <label htmlFor="" className="ml-2">
+              Last Name
+            </label>
+            <Input
+              className="bg-gray-100 p-2 rounded-lg outline-none"
+              readOnly
+              value={user?.lastName}
+            />
+          </div>
+
+          <div className="flex rounded-lg flex-col p-2 gap-1">
+            <label htmlFor="" className="ml-2">
+              Username
+            </label>
+            <Input
+              className="bg-gray-100 p-2 rounded-lg outline-none"
+              readOnly
+              value={user?.username}
+            />
+          </div>
+
+          <div className="flex rounded-lg flex-col p-2 gap-1">
+            <label htmlFor="" className="ml-2">
+              Email
+            </label>
+            <Input
+              className="bg-gray-100 p-2 rounded-lg outline-none"
+              readOnly
+              value={user?.email}
+            />
+          </div>
+
+          <div className="flex md:col-span-2 rounded-lg flex-col p-2 gap-1">
+            <label htmlFor="" className="ml-2">
+              Bio
+            </label>
+            <Textarea
+              className="bg-gray-100 p-2 resize-none rounded-lg outline-none"
+              readOnly
+              value={user?.bio}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
