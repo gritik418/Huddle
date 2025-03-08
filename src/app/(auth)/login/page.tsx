e4902c9ecc0c1fe4c1bfbdec6886caa1d1435aec
Ritik <160255700+gritik418@users.noreply.github.com>
2025-03-09 00:03:00 +0530

    Updated store.ts

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import Image from "next/image";
 import Link from "next/link";
-import { LoginResponse, useUserLoginMutation } from "@/features/api/authApi";
+import {
+  LoginResponse,
+  useUserLoginMutation,
+} from "../../../features/api/authApi";
 import { useRouter } from "next/navigation";
 import { JSX, useState } from "react";
 import { useForm } from "react-hook-form";
