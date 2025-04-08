c521a276bbcd2db06ae269d8ababf1c55e105105
Ritik <160255700+gritik418@users.noreply.github.com>
2025-04-08 12:17:50 +0530

    Accept or decline channel invite feature completed

--- src/components/ChannelInviteItem/ChannelInviteItem.tsx
+++ src/components/ChannelInviteItem/ChannelInviteItem.tsx
@@ -1,27 +1,138 @@
+import {
+  ChannelInviteApiResponse,
+  useAcceptChannelInviteMutation,
+  useDeclineChannelInviteMutation,
+} from "@/features/api/channelInviteApi";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import Image from "next/image";
-import { JSX } from "react";
+import { JSX, useState } from "react";
+import { Bounce, toast } from "react-toastify";
+import Spinner from "../Spinner/Spinner";
 
 const ChannelInviteItem = ({
   invite,
 }: {
   invite: ChannelInvite;
 }): JSX.Element => {
+  console.log(invite);
   const sender = invite.senderId;
   const channel = invite.channelId;
+  const [acceptInvite] = useAcceptChannelInviteMutation();
+  const [declineInvite] = useDeclineChannelInviteMutation();
+  const [acceptLoading, setAcceptLoading] = useState<boolean>(false);
+  const [declineLoading, setDeclineLoading] = useState<boolean>(false);
+
+  const handleAcceptInvite = async (): Promise<void> => {
+    try {
+      setAcceptLoading(true);
+      const { data, error } = await acceptInvite(invite._id);
+      setAcceptLoading(false);
+      if (error) {
+        const errorResponse = error as FetchBaseQueryError;
+        const parsedError = errorResponse?.data as ChannelInviteApiResponse;
+
+        toast.error(parsedError.message, {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      } else if (data) {
+        toast.success(data.message, {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.success("Something went wrong.", {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+  };
+
+  const handleDeclineInvite = async (): Promise<void> => {
+    try {
+      setDeclineLoading(true);
+      const { data, error } = await declineInvite(invite._id);
+      setDeclineLoading(false);
+      if (error) {
+        const errorResponse = error as FetchBaseQueryError;
+        const parsedError = errorResponse?.data as ChannelInviteApiResponse;
+
+        toast.error(parsedError.message, {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      } else if (data) {
+        toast.success(data.message, {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.success("Something went wrong.", {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+  };
 
   return (
     <div className="bg-white border shadow-sm rounded-2xl p-5 mb-4 transition hover:shadow-md">
       <div className="flex items-center gap-3 mb-4">
         <Image
-          src={sender.profilePicture || "/default-profile.jpg"}
-          alt={sender.username}
+          src={sender?.profilePicture || "/default-profile.jpg"}
+          alt={sender?.username}
           height={50}
           width={50}
           className="w-12 h-12 rounded-full object-cover"
         />
         <div>
           <p className="text-sm text-gray-800 font-semibold">
-            {sender.firstName} {sender.lastName || ""}
+            {sender.firstName} {sender?.lastName || ""}
           </p>
           <p className="text-xs text-gray-500">@{sender.username}</p>
           <p className="text-xs text-gray-400 mt-1 italic">
@@ -45,11 +156,17 @@ const ChannelInviteItem = ({
       </div>
 
       <div className="flex justify-end gap-2 mt-4">
-        <button className="px-4 py-1.5 text-sm rounded-lg border border-red-300 text-red-600 hover:bg-red-50 transition">
-          Decline
+        <button
+          onClick={handleDeclineInvite}
+          className="h-8 w-16 flex items-center justify-center outline-none text-sm rounded-lg border text-white bg-red-500 hover:bg-red-600 transition"
+        >
+          {declineLoading ? <Spinner variant={null} /> : "Decline"}
         </button>
-        <button className="px-4 py-1.5 text-sm rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition">
-          Accept
+        <button
+          onClick={handleAcceptInvite}
+          className="h-8 w-16 flex items-center justify-center outline-none text-sm rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition"
+        >
+          {acceptLoading ? <Spinner variant={null} /> : "Accept"}
         </button>
       </div>
     </div>
